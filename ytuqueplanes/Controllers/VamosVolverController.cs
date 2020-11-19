82e39cb15e4d057128888b4f5cd4b42fa97b5f38
kjayru <wiltinoco@gmail.com>
2020-11-19 18:28:10 -0500

    vamos a volver

---
+++ ytuqueplanes/Controllers/VamosVolverController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using EntidadesData;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ytuqueplanes.Models;
+using System.Configuration;
+
+namespace ytuqueplanes.Controllers
+{
+    public class VamosVolverController : Controller
+    {
+        ytuqueplanesDBEntities1 db = new ytuqueplanesDBEntities1();
+
+        public VamosVolverController()
+        {
+            ViewBag.hostStaticUrl = ConfigurationManager.AppSettings["staticURL"];
+            var prov = db.provincias.Where(c => c.estado == 1 && c.id != 26).Select(p => new { p.id, p.nombre, p.slug, p.region_id }).ToList();
+
+            List<Provincias> pr = new List<Provincias>();
+            for (var i = 0; i < prov.Count(); i++)
+            {
+
+                var regID = prov[i].region_id;
+                string regname = "";
+                var reg = db.regions.Where(c => c.id == regID).Select(q => new { q.nombre }).FirstOrDefault();
+
+                if (reg != null)
+                {
+                    regname = reg.nombre;
+                }
+                pr.Add(new Provincias
+                {
+                    id = prov[i].id,
+                    slug = prov[i].slug,
+                    nombre = prov[i].nombre,
+                    region = regname
+
+                });
+
+            }
+
+            ViewBag.gprovincias = pr;
+
+            var seo = db.seos.Where(c => c.id == 2).Select(p => new { p.og_title, p.og_description, p.og_image, p.keywords }).FirstOrDefault();
+            //seo
+            ViewBag.seotitle = seo.og_title;
+            ViewBag.seotype = seo.og_description;
+            ViewBag.seourl = System.Web.HttpContext.Current.Request.Url.AbsoluteUri;
+            ViewBag.seoimagen = seo.og_image;
+            ViewBag.seodescripcion = seo.og_description;
+            ViewBag.keywords = seo.keywords;
+        }
+        // GET: VamosVolver
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
